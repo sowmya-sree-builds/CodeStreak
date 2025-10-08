e7fd0547c2481bd07cffee5bc4ce8a61a527ef35
sweetblink <its.sowmyasree@gmail.com>
2025-10-08 22:58:57 +0530

    Day 42: Implemented Remove Duplicates from list without built-ins

--- Coding Challenges/Day_42_Remove_Duplicates.py
+++ Coding Challenges/Day_42_Remove_Duplicates.py
@@ -1,1 +1,16 @@
 # Day 42: Remove Duplicates from List using Set 
+
+def remove_duplicates(lst):
+    seen = set()
+    result = []
+    for item in lst:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+
+# 🧩 Test
+nums = [1, 2, 2, 3, 4, 4, 5]
+print("\nOriginal List:", nums)
+print("After Removing Duplicates:", remove_duplicates(nums))
