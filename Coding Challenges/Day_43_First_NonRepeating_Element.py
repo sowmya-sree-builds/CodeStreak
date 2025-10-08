e3832f6f38c2e55666bf52d7617e5b2c7bfd5d73
sweetblink <its.sowmyasree@gmail.com>
2025-10-08 22:58:59 +0530

    Day 43: Added program to find First Non-Repeating Element using Dictionary

--- Coding Challenges/Day_43_First_NonRepeating_Element.py
+++ Coding Challenges/Day_43_First_NonRepeating_Element.py
@@ -1,1 +1,18 @@
 # Day 43: Find First Non-Repeating Element using Dictionary 
+
+def first_non_repeating(lst):
+    freq = {}
+    for item in lst:
+        freq[item] = freq.get(item, 0) + 1
+
+    for item in lst:
+        if freq[item] == 1:
+            return item
+    return None
+
+
+# 🧩 Test
+elements = [9, 4, 9, 6, 7, 4]
+print("\nList:", elements)
+print("First Non-Repeating Element:", first_non_repeating(elements))
+
